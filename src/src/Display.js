a64732ae9bd82ce9aac2e11c2a27e0bcaf274748
James Grieve <jgrieve@ualberta.ca>
2023-04-03 20:34:27 -0600

    Query parameters;

--- src/src/Display.js
+++ src/src/Display.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { useLocation } from 'react-router-dom';
+import { useMemo } from 'react';
 
 function Display() {
+  const name = new URLSearchParams(useLocation().search).get("name")
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Display.
+          Welcome to Display, {name}.
         </p>
         <a
           className="App-link"
